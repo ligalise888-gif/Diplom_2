6027f0e931e8b258cde97bb9a8fc8f63e7880154
Konstantin <ligalise888@gmail.com>
2026-08-15 21:54:52 +0200

    Убраны проверки из фикстур

--- conftest.py
+++ conftest.py
@@ -6,17 +6,12 @@ from helpers import generate_user_data
 
 
 @pytest.fixture
-def user_data():
-    return generate_user_data()
-
-
-@pytest.fixture
-def created_user(user_data):
-    response = requests.post(Urls.BASE_URL + Urls.REGISTER, json=user_data)
-    assert response.status_code == 200, f'Не удалось создать пользователя: {response.text}'
+def created_user():
+    user = generate_user_data()
+    response = requests.post(Urls.BASE_URL + Urls.REGISTER, json=user)
     access_token = response.json().get('accessToken')
 
-    yield user_data, access_token
+    yield user, access_token
 
     if access_token:
         requests.delete(Urls.BASE_URL + Urls.USER, headers={'Authorization': access_token})
@@ -25,7 +20,5 @@ def created_user(user_data):
 @pytest.fixture
 def ingredient_ids():
     response = requests.get(Urls.BASE_URL + Urls.INGREDIENTS)
-    assert response.status_code == 200, f'Не удалось получить список ингредиентов: {response.text}'
-    data = response.json().get('data', [])
-    assert len(data) >= 2, 'В ответе меньше двух ингредиентов'
+    data = response.json()['data']
     return [data[0]['_id'], data[1]['_id']]
